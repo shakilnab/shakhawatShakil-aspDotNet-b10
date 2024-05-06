37fe9e82caa08cc9130f00f0f3c39d0b18320815
shakilnab <shakilnab21@gmail.com>
2024-05-06 10:36:55 +0600

    .

---
+++ src/Assignment_One_Solution/Assignment1/Program.cs
@@ -0,0 +1,23 @@
+﻿using Assignment1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Student student = new Student();
+        student.Id = 11808021;
+        student.Name = "Shakil";
+
+        Console.WriteLine(JsonFormatter.Convert(student));
+    }
+}
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int[,] TwoDArray { get; set; }
+    public int[,,] ThreeDArray { get; set; }
+}
